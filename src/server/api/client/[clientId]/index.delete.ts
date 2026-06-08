ed42e1385b683dc6fe6e4c33b7754116d58b969b
Pommy <tm_git@93m.org>
2026-06-08 22:04:24 +0200

    Port forwarding: deleting port forwarding rules when deleting client

--- src/server/api/client/[clientId]/index.delete.ts
+++ src/server/api/client/[clientId]/index.delete.ts
@@ -18,8 +18,40 @@ export default definePermissionEventHandler(
     const client = await Database.clients.get(clientId);
     checkPermissions(client);
 
+    if (!client) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: 'Client not found',
+      });
+    }
+
     await Database.clients.delete(clientId);
     await WireGuard.saveConfig();
+
+    const baseUrl = THIRD_PARTY_ENV.PORT_FORWARDING_URL;
+
+    if (!baseUrl) {
+      return [];
+    }
+
+    const updatedEntry: PortForwardingDefinition = {
+      ipv4: client.ipv4Address,
+      ports: [],
+    };
+
+    try {
+      await $fetch(`${baseUrl}/ports/`, {
+        method: 'PUT',
+        body: [updatedEntry],
+      });
+    } catch (e) {
+      SERVER_DEBUG('Failed to update port forwarding data: ', e);
+      throw createError({
+        statusCode: 502,
+        statusMessage: 'Failed to update port forwarding data',
+      });
+    }
+
     return { success: true };
   }
 );
